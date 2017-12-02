c9750e520cb63ff300e3e5e28c3b6c871a78dfd0
Debaraj Barua <debaraj.barua@smail.inf.h-brs.de>
2017-12-02 19:19:59 +0100

    updated complete assignment

---
+++ Assignment08/NN_DebarajBarua_NareshKumarGurulingan_271117/NN_DebarajBarua_NareshKumarGurulingan_271117_src/MindMap/Radial-Basis Function.mm
@@ -0,0 +1,102 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1512236260917" ID="ID_481246535" MODIFIED="1512236456336" TEXT="Radial-Basis Function">
+<node CREATED="1512236456318" ID="ID_1992914386" MODIFIED="1512236460116" POSITION="right" TEXT="Netwotk">
+<node CREATED="1512236310433" HGAP="50" ID="ID_755027395" MODIFIED="1512237719019" TEXT="Structure" VSHIFT="5">
+<node CREATED="1512236341106" ID="ID_966820942" MODIFIED="1512236349040" TEXT="Input Layer">
+<node CREATED="1512236371360" ID="ID_1738891852" MODIFIED="1512236385196" TEXT="No weights"/>
+</node>
+<node CREATED="1512236349760" ID="ID_1037559379" MODIFIED="1512236354578" TEXT="Hidden Layer">
+<node CREATED="1512236483494" ID="ID_1762202914" MODIFIED="1512236492471" TEXT="unsupervised"/>
+<node CREATED="1512236393217" ID="ID_114487660" MODIFIED="1512236401093" TEXT="Non-linear"/>
+</node>
+<node CREATED="1512236354996" ID="ID_246519402" MODIFIED="1512236362384" TEXT="Output Layer">
+<node CREATED="1512236410512" ID="ID_141458870" MODIFIED="1512236414956" TEXT="Linear"/>
+</node>
+</node>
+<node CREATED="1512236468025" HGAP="27" ID="ID_181780346" MODIFIED="1512236666938" TEXT="Learning" VSHIFT="37">
+<node CREATED="1512236630687" ID="ID_1269473100" MODIFIED="1512236669522" TEXT="Generalized RBFN" VSHIFT="-34">
+<node CREATED="1512236675114" ID="ID_902036755" MODIFIED="1512236683968" TEXT="Universal Approximation"/>
+<node CREATED="1512236686457" ID="ID_1853283501" MODIFIED="1512236692910" TEXT="kernal Regression"/>
+</node>
+<node CREATED="1512236640160" HGAP="22" ID="ID_593756779" MODIFIED="1512236671170" TEXT="K-Means Clustering" VSHIFT="29">
+<node CREATED="1512236700097" ID="ID_1320357312" MODIFIED="1512236714889" TEXT="Algorithm">
+<node CREATED="1512236748684" ID="ID_1317401176" MODIFIED="1512236861455" TEXT="Step 1: k-initial means"/>
+<node CREATED="1512236753656" ID="ID_638899672" MODIFIED="1512236868160" TEXT="Step 2: k-clusters centered around the k -means"/>
+<node CREATED="1512236775652" ID="ID_1776744168" MODIFIED="1512236875573" TEXT="Step 3: Centroid of the cluster becomes new mean"/>
+<node CREATED="1512236876580" ID="ID_527098573" MODIFIED="1512236910528" TEXT="Step 4: Repeat steps 2,3 until the centers do not move any more"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1512237067192" HGAP="17" ID="ID_1497441236" MODIFIED="1512237712115" POSITION="left" TEXT="Cover&apos;s Theorem" VSHIFT="-16">
+<node CREATED="1512237283353" ID="ID_416035967" MODIFIED="1512237323499" TEXT="Separability of patterns">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A complex pattern-classification problem, cast in a high-dimensional space nonlinearly,
+    </p>
+    <p>
+      is more likely to be linearly separable than in a low-dimensional space, provided that the
+    </p>
+    <p>
+      space is not densely populated.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1512237326724" ID="ID_1729233606" MODIFIED="1512237416893" TEXT="Example">
+<node CREATED="1512237417865" ID="ID_985298984" MODIFIED="1512237420373" TEXT="XOR"/>
+</node>
+</node>
+<node CREATED="1512237088905" HGAP="16" ID="ID_563260135" MODIFIED="1512237707819" POSITION="left" TEXT="Interpolation Problem" VSHIFT="30">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Given a set of N different points and a corresponding set of N
+    </p>
+    <p>
+      real numbers , find a function that satisfies the interpolation condition
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      F(xi) = di, i = 1, 2, ..., N
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1512237551482" ID="ID_1600753366" MODIFIED="1512237569552" TEXT="Micchelli&apos;s Theorem">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Let be a set of distinct points in . Then the N-by-N interpolation matrix , whose
+    </p>
+    <p>
+      ij-th element is , is nonsingular
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1512237576874" ID="ID_1706504299" MODIFIED="1512237584850" TEXT="Multiquadratics"/>
+<node CREATED="1512237585660" ID="ID_21186227" MODIFIED="1512237593694" TEXT="Inverse multiquadratics"/>
+<node CREATED="1512237594454" ID="ID_1686367512" MODIFIED="1512237603647" TEXT="Gaussian function"/>
+</node>
+<node CREATED="1512237633905" ID="ID_1518066026" MODIFIED="1512237662272" TEXT="Learning Process">
+<node CREATED="1512237664452" ID="ID_1538192630" MODIFIED="1512237668537" TEXT="Training"/>
+<node CREATED="1512237668993" ID="ID_510752222" MODIFIED="1512237684480" TEXT="Generalization"/>
+</node>
+</node>
+</node>
+</map>
